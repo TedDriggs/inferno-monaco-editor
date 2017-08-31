f3e1679b0773143e08f0fcc51b2f57d040a2357a
Ted Driggs <ted.driggs@outlook.com>
2017-08-31 14:57:47 -0700

    Update demo

--- examples/Client.tsx
+++ examples/Client.tsx
@@ -3,7 +3,7 @@ import InfernoComponent from 'inferno-component';
 
 import MonacoEditor from 'src';
 
-export default class Client extends InfernoComponent<any, never> {
+export default class Client extends InfernoComponent<any, any> {
     private editor?: MonacoEditor;
     private handleChange: { (newValue: string): void };
 
@@ -11,23 +11,36 @@ export default class Client extends InfernoComponent<any, never> {
         super(props);
         window.addEventListener('resize', _evt => { if (this.editor) this.editor.layout(); });
         this.handleChange = nv => {
-            console.log(nv);
+            if (nv.includes('readonly!')) {
+                this.setState({
+                    readOnly: true
+                });
+            }
         }
+
+        this.state = {
+            readOnly: false
+        };
     }
 
     render() {
         return (
-            <MonacoEditor 
-                ref={node => this.editor = node}
-                width="100%" 
-                height="100%" 
-                readOnly={false}
-                value="if (Flow.server.ipaddr.isRFC1918) Application('int').commit();"
-                style={{
-                    border: '1px solid gray',
-                    margin: '5px',
-                }}
-                onChange={this.handleChange} />
+            <div style={{ width: '100%', height: '100%', display: 'flex' }}>
+                <div style={{ width: '30%', height: '100%' }}>
+                    <p>Type `readonly!` to disable the editor (demonstrates prop updates)</p>
+                </div>
+                <MonacoEditor 
+                    ref={node => this.editor = node}
+                    width="80%"
+                    height="100%"
+                    readOnly={this.state.readOnly}
+                    value="if (Flow.server.ipaddr.isRFC1918) Application('int').commit();"
+                    style={{
+                        border: '1px solid gray',
+                        margin: '5px',
+                    }}
+                    onChange={this.handleChange} />
+            </div>
         );
     }
 }
